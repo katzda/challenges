b1dbf0281b8ebe30b1b94274fbcb372d9380865f
Daniel Katz <danekkatz@gmail.com>
2023-12-14 18:31:47 +0000

    fixed a bug

--- SSSGame/script.js
+++ SSSGame/script.js
@@ -1,23 +1,20 @@
-function MemoryGame(){
-    let table = [];
-    let seedEl = document.getElementById('seed');
-    let seeds;
-    let step = 0;
-    let roundNumber = 0;
+function MemoryGame() {
 
+    let table = [];
     let noChairsEl = document.getElementById('noChairs');
-    let UpdateUI_NoChairs = function(){
+    let UpdateUI_NoChairs = function () {
         noChairsEl.innerText = table.length;
     }
 
     let roundEl = document.getElementById('round');
-    let UpdateUI_Round = function(){
+    let roundNumber = 0;
+    let UpdateUI_Round = function () {
         roundEl.innerText = roundNumber;
     }
 
     let isVisible = false;
     let tableEl = document.getElementById('table');
-    let UpdateUI_TableVisibility = function(){
+    let UpdateUI_TableVisibility = function () {
         tableEl.style.display = isVisible ? 'table' : 'none';
     }
 
@@ -42,6 +39,7 @@ function MemoryGame(){
         tableHeadingNextSeatPointerEl.classList.add('table-cell');
 
         tableHeadingNumberEl.innerText = '#';
+
         tableHeadingNameEl.innerText = 'Name';
         tableHeadingCountryEl.innerText = 'Country';
         tableHeadingFruitEl.innerText = 'Fruit';
@@ -71,7 +69,7 @@ function MemoryGame(){
             let tableRowEl = document.createElement('tr');
             let tableCellPointer = document.createElement('td');
 
-            if(i == nextFreeSeat){
+            if (i == nextFreeSeat) {
                 tableCellPointer.innerText = '<---';
             }
 
@@ -81,7 +79,7 @@ function MemoryGame(){
                 tableCellFruitEl.innerText = person['fruit'];
                 tableCellAnimalEl.innerText = person['animal'];
                 tableCellSportEl.innerText = person['sport'];
-            }else{
+            } else {
                 tableCellNameEl.innerText = '-';
                 tableCellCountryEl.innerText = '-';
                 tableCellFruitEl.innerText = '-';
@@ -89,7 +87,7 @@ function MemoryGame(){
                 tableCellSportEl.innerText = '-';
             }
 
-            tableCellNumberEl.innerText = i+1;
+            tableCellNumberEl.innerText = i + 1;
 
             tableRowEl.appendChild(tableCellNumberEl);
             tableRowEl.appendChild(tableCellNameEl);
@@ -115,38 +113,37 @@ function MemoryGame(){
         tableEl.classList.add('w-full');
     }
 
-    function UpdateUI(){
+    function UpdateUI() {
         UpdateUI_NoChairs();
         UpdateUI_Table();
-        UpdateUI_Round();
         UpdateUI_TableVisibility();
+        UpdateUI_Round();
     }
-
-    let seedErrorEl = document.getElementById('seedErrorMessage');
-    let nameEl = document.getElementById('name');
-    let sportEl = document.getElementById('sport');
-    let fruitEl = document.getElementById('fruit');
-    let countryEl = document.getElementById('country');
-    let animalEl = document.getElementById('animal');
 
     let vocab = {
-        'names' : ['Jack','Emma','Liam','Olivia','Noah','Ava','Lucas','Sophia','Ethan','Isabella','Mason','Mia','Logan','Charlotte','Jacob','Amelia','Aiden','Nicole','Benjamin','Evelyn','William','Abigail','James','Emily','Oliver','Elizabeth','Elijah','Avery','Alexander','Sofia','Michael','Ella','Daniel','Scarlett','Henry','Grace','Jackson','Chloe','Sebastian','Lily','Matthew','Addison','Samuel','Natalie','David','Victoria','Joseph','Zoey','Carter','Penelope','Gabriel','Layla','Luke','Riley','Anthony','Aubrey','Isaac','Nora','Dylan','Skylar','Wyatt','Hannah','Andrew','Zoey','Joshua','Brooklyn','Christopher','Savannah','Grayson','Audrey','Nathan','Leah','Eli','Claire','Samuel','Stella','Owen','Agnes','Connor','Ellie','Landon','Maya','Ryan','Violet','Isaiah','Aria','Christian','Aurora','Hunter','Sarah','Cameron','Caroline','Thomas','Naomi','Charles','Anna','Julian','Samantha','Aaron','Eliana'],
-        'sports' : ['Football','Basketball','American Football','Baseball','Tennis','Golf','Rugby','Cricket','Volleyball','Table Tennis','Badminton','Hockey','Swimming','Running','Boxing','Martial Arts','Wrestling','Gymnastics','Cycling','Surfing','Skateboarding','Snowboarding','Skiing','Weightlifting','Archery','Shooting','Fencing','Diving','Sailing','Rowing','Canoeing/Kayaking','Equestrian','Triathlon','Triathlon','Pole Vault','High Jump','Long Jump','Javelin Throw','Hammer Throw','Discus Throw','Decathlon/Heptathlon','Bobsleigh','Bungee Jumping','Rock Climbing','Mountaineering','Paragliding','Kite Surfing','Windsurfing','Handball','Lacrosse'],
-        'fruits' : ['Apple','Orange','Banana','Pear','Strawberry','Grape','Blueberry','Raspberry','Pineapple','Watermelon','Kiwi','Mango','Papaya','Peach','Cherry','Plum','Lemon','Lime','Avocado','Fig','Cranberry','Blackberry','Cantaloupe','Honeydew melon','Grapefruit','Pomegranate','Coconut','Passion fruit','Lychee','Dragon fruit','Guava','Persimmon','Tangerine','Nectarine','Apricot','Jackfruit','Star fruit','Elderberry','Raspberry','Boysenberry','Mulberry','Gooseberry','Ackee','Plantain','Date','Quince','Rambutan','Longan','Durian','Kiwano'],
-        'countries' : ['Afghanistan','Albania','Algeria','Andorra','Angola','Antigua and Barbuda','Argentina','Armenia','Australia','Austria','Azerbaijan','Bahamas','Bahrain','Bangladesh','Barbados','Belarus','Belgium','Belize','Benin','Bhutan','Bolivia','Bosnia and Herzegovina','Botswana','Brazil','Brunei','Bulgaria','Burkina Faso','Burundi','Cabo Verde','Cambodia','Cameroon','Canada','Central African Republic','Chad','Chile','China','Colombia','Comoros','Congo','Costa Rica','Croatia','Cuba','Cyprus','Czechia','Democratic Republic of the Congo','Denmark','Djibouti','Dominica','Dominican Republic','Ecuador','Egypt','El Salvador','Equatorial Guinea','Eritrea','Estonia','Eswatini','Ethiopia','Fiji','Finland','France','Gabon','Gambia','Georgia','Germany','Ghana','Greece','Grenada','Guatemala','Guinea','Guinea-Bissau','Guyana','Haiti','Honduras','Hungary','Iceland','India','Indonesia','Iran','Iraq','Ireland','Israel','Italy','Ivory Coast','Jamaica','Japan','Jordan','Kazakhstan','Kenya','Kiribati','Kosovo','Kuwait','Kyrgyzstan','Laos','Latvia','Lebanon','Lesotho','Liberia','Libya','Liechtenstein'],
-        'animals' : ['Dog','Cat','Elephant','Lion','Tiger','Giraffe','Zebra','Hippopotamus','Rhinoceros','Gorilla','Chimpanzee','Bear','Panda','Koala','Kangaroo','Cheetah','Jaguar','Leopard','Wolf','Fox','Deer','Moose','Elk','Bison','Yak','Camel','Alpaca','Llama','Horse','Donkey','Mule','Goat','Sheep','Cow','Bull','Pig','Boar','Warthog','Rhinoceros','Hyena','Wildebeest','Antelope','Gazelle','Ostrich','Emu','Flamingo','Penguin','Toucan','Parrot','Eagle','Hawk','Falcon','Owl','Pelican','Seagull','Swan','Duck','Goose','Chicken','Rooster','Pigeon','Sparrow','Hummingbird','Crow','Magpie','Blue Jay','Cardinal','Robin','Goldfish','Clownfish','Angelfish','Betta fish','Swordfish','Shark','Dolphin','Whale','Seal','Sea lion','Walrus','Otter','Beaver','Platypus','Crocodile','Alligator','Turtle','Tortoise','Snake','Lizard','Gecko','Iguana','Chameleon','Frog','Toad','Salamander','Newt','Tarantula','Scorpion','Spider','Centipede','Millipede'],
+        'name': ['Jack', 'Emma', 'Liam', 'Olivia', 'Noah', 'Ava', 'Lucas', 'Sophia', 'Ethan', 'Isabella', 'Mason', 'Mia', 'Logan', 'Charlotte', 'Jacob', 'Amelia', 'Aiden', 'Nicole', 'Benjamin', 'Evelyn', 'William', 'Abigail', 'James', 'Emily', 'Oliver', 'Elizabeth', 'Elijah', 'Avery', 'Alexander', 'Sofia', 'Michael', 'Ella', 'Daniel', 'Scarlett', 'Henry', 'Grace', 'Jackson', 'Chloe', 'Sebastian', 'Lily', 'Matthew', 'Addison', 'Samuel', 'Natalie', 'David', 'Victoria', 'Joseph', 'Zoey', 'Carter', 'Penelope', 'Gabriel', 'Layla', 'Luke', 'Riley', 'Anthony', 'Aubrey', 'Isaac', 'Nora', 'Dylan', 'Skylar', 'Wyatt', 'Hannah', 'Andrew', 'Zoey', 'Joshua', 'Brooklyn', 'Christopher', 'Savannah', 'Grayson', 'Audrey', 'Nathan', 'Leah', 'Eli', 'Claire', 'Samuel', 'Stella', 'Owen', 'Agnes', 'Connor', 'Ellie', 'Landon', 'Maya', 'Ryan', 'Violet', 'Isaiah', 'Aria', 'Christian', 'Aurora', 'Hunter', 'Sarah', 'Cameron', 'Caroline', 'Thomas', 'Naomi', 'Charles', 'Anna', 'Julian', 'Samantha', 'Aaron', 'Eliana'],
+        'sport': ['Football', 'Basketball', 'American Football', 'Baseball', 'Tennis', 'Golf', 'Rugby', 'Cricket', 'Volleyball', 'Table Tennis', 'Badminton', 'Hockey', 'Swimming', 'Running', 'Boxing', 'Martial Arts', 'Wrestling', 'Gymnastics', 'Cycling', 'Surfing', 'Skateboarding', 'Snowboarding', 'Skiing', 'Weightlifting', 'Archery', 'Shooting', 'Fencing', 'Diving', 'Sailing', 'Rowing', 'Canoeing/Kayaking', 'Equestrian', 'Triathlon', 'Pole Vault', 'High Jump', 'Long Jump', 'Javelin Throw', 'Hammer Throw', 'Discus Throw', 'Decathlon/Heptathlon', 'Bobsleigh', 'Bungee Jumping', 'Rock Climbing', 'Mountaineering', 'Paragliding', 'Kite Surfing', 'Windsurfing', 'Handball', 'Lacrosse'],
+        'fruit': ['Apple', 'Orange', 'Banana', 'Pear', 'Strawberry', 'Grape', 'Blueberry', 'Raspberry', 'Pineapple', 'Watermelon', 'Kiwi', 'Mango', 'Papaya', 'Peach', 'Cherry', 'Plum', 'Lemon', 'Lime', 'Avocado', 'Fig', 'Cranberry', 'Blackberry', 'Cantaloupe', 'Honeydew melon', 'Grapefruit', 'Pomegranate', 'Coconut', 'Passion fruit', 'Lychee', 'Dragon fruit', 'Guava', 'Persimmon', 'Tangerine', 'Nectarine', 'Apricot', 'Jackfruit', 'Star fruit', 'Elderberry', 'Raspberry', 'Boysenberry', 'Mulberry', 'Gooseberry', 'Ackee', 'Plantain', 'Date', 'Quince', 'Rambutan', 'Longan', 'Durian', 'Kiwano'],
+        'country': ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Democratic Republic of the Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Ivory Coast', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein'],
+        'animal': ['Dog', 'Cat', 'Elephant', 'Lion', 'Tiger', 'Giraffe', 'Zebra', 'Hippopotamus', 'Rhinoceros', 'Gorilla', 'Chimpanzee', 'Bear', 'Panda', 'Koala', 'Kangaroo', 'Cheetah', 'Jaguar', 'Leopard', 'Wolf', 'Fox', 'Deer', 'Moose', 'Elk', 'Bison', 'Yak', 'Camel', 'Alpaca', 'Llama', 'Horse', 'Donkey', 'Mule', 'Goat', 'Sheep', 'Cow', 'Bull', 'Pig', 'Boar', 'Warthog', 'Rhinoceros', 'Hyena', 'Wildebeest', 'Antelope', 'Gazelle', 'Ostrich', 'Emu', 'Flamingo', 'Penguin', 'Toucan', 'Parrot', 'Eagle', 'Hawk', 'Falcon', 'Owl', 'Pelican', 'Seagull', 'Swan', 'Duck', 'Goose', 'Chicken', 'Rooster', 'Pigeon', 'Sparrow', 'Hummingbird', 'Crow', 'Magpie', 'Blue Jay', 'Cardinal', 'Robin', 'Goldfish', 'Clownfish', 'Angelfish', 'Betta fish', 'Swordfish', 'Shark', 'Dolphin', 'Whale', 'Seal', 'Sea lion', 'Walrus', 'Otter', 'Beaver', 'Platypus', 'Crocodile', 'Alligator', 'Turtle', 'Tortoise', 'Snake', 'Lizard', 'Gecko', 'Iguana', 'Chameleon', 'Frog', 'Toad', 'Salamander', 'Newt', 'Tarantula', 'Scorpion', 'Spider', 'Centipede', 'Millipede'],
     };
+    let vocabTopics = Object.keys(vocab);
+    let UI_elements = vocabTopics.reduce((acc, cur) => {
+        acc[cur] = document.getElementById(cur);
+        return acc;
+    }, {});
 
-    function zeroToArrayLengthMinusOne(length, seed){
-        return ((seed+step)%length);
+    let step = 0;
+    function zeroToArrayLengthMinusOne(length, seed) {
+        return ((seed + step) % length);
     }
 
-    function zeroToArrayLengthMinusOneLess(length, seed){
-        return ((seed+step+0.00001)%length);
+    function zeroToArrayLengthMinusOneLess(length, seed) {
+        return ((seed + step + 0.00001) % length);
     }
 
     function toRad(length, seed) {
-        return Math.PI * zeroToArrayLengthMinusOneLess(length*2,seed) / (length*2);
+        return Math.PI * zeroToArrayLengthMinusOneLess(length * 2, seed) / (length * 2);
     }
 
     /** returns 0 or -1 */
@@ -156,35 +153,34 @@ function MemoryGame(){
 
     /** returns 0 or -1 */
     function predictableRandom(length, seed) {
-        return Math.abs(zeroToNegOne(length-1, seed) * (length-1) + zeroToArrayLengthMinusOne(length-1, seed));
+        return Math.abs(zeroToNegOne(length - 1, seed) * (length - 1) + zeroToArrayLengthMinusOne(length - 1, seed));
     }
 
     function getRandomElement(arr, seed) {
         return arr[predictableRandom(arr.length, seed)];
     }
 
+    let seeds;
     function Generate() {
         return vocabTopics.reduce((acc, cur, index) => {
-            acc[cur] = getRandomElement(vocab[topic], seeds[index]);
+            acc[cur] = getRandomElement(vocab[cur], seeds[index]);
             return acc;
         }, {});
     }
 
-    function UpdateUI_Person(person){
-        nameEl.innerText = person['name'] ?? "";
-        countryEl.innerText = person['country'] ?? "";
-        fruitEl.innerText = person['fruit'] ?? "";
-        animalEl.innerText = person['animal'] ?? "";
-        sportEl.innerText = person['sport'] ?? "";
+    function UpdateUI_Person(person) {
+        vocabTopics.forEach((topic)=>{
+            UI_elements[topic].innerText = person[topic] ?? "";
+        });
     }
 
     let nextFreeSeat = 0;
-    function Remember(person){
+    function Remember(person) {
         table[nextFreeSeat] = person;
-        nextFreeSeat = nextFreeSeat < table.length-1 ? nextFreeSeat + 1 : 0;
+        nextFreeSeat = nextFreeSeat < table.length - 1 ? nextFreeSeat + 1 : 0;
     }
 
-    this.next = function(){
+    this.next = function () {
         step++;
         roundNumber++;
         let person = Generate();
@@ -193,78 +189,78 @@ function MemoryGame(){
         UpdateUI();
     }
 
-    this.addChair = function(){
+    this.addChair = function () {
         table[table.length] = null;
-        if(table[nextFreeSeat] != null){
+        if (table[nextFreeSeat] != null) {
             nextFreeSeat = table.length - 1;
         }
         UpdateUI();
     }
 
-    this.removeChair = function(){
+    this.removeChair = function () {
         table.length -= 1;
-        if(nextFreeSeat = table.length){
+        if (nextFreeSeat = table.length) {
             nextFreeSeat = 0;
         }
         UpdateUI();
     }
 
-    function calcSeeds(finalNumberOfTheCombination, arraySizes, currentDepth, startingIndex, counter){
-        if(typeof currentDepth === 'undefined'){
+    function calcSeeds(finalNumberOfTheCombination, arraySizes, currentDepth, startingIndex, counter) {
+        if (typeof currentDepth === 'undefined') {
             currentDepth = 0;
         }
-        if(typeof startingIndex === 'undefined'){
+        if (typeof startingIndex === 'undefined') {
             startingIndex = 0;
         }
-        if(typeof counter === 'undefined'){
+        if (typeof counter === 'undefined') {
             var counter = 0;
         }
-        for(let i=startingIndex; i < arraySizes[currentDepth]; i++){
-            if(currentDepth == arraySizes.length -1){
-                if(counter == finalNumberOfTheCombination){
+        for (let i = startingIndex; i < arraySizes[currentDepth]; i++) {
+            if (currentDepth == arraySizes.length - 1) {
+                if (counter == finalNumberOfTheCombination) {
                     results = [];
                     results.length = arraySizes.length;
                     results[currentDepth] = i;
                     return results;
-                }else{
+                } else {
                     counter++;
                     continue;
                 }
-            }else{
-                let results = calcSeeds(finalNumberOfTheCombination, arraySizes, currentDepth+1, 0, counter);
-                if(typeof results == 'number'){
+            } else {
+                let results = calcSeeds(finalNumberOfTheCombination, arraySizes, currentDepth + 1, 0, counter);
+                if (typeof results == 'number') {
                     counter = results;
-                }else{
+                } else {
                     results[currentDepth] = i;
                     return results;
                 }
             }
         }
-        if(counter <= finalNumberOfTheCombination){
+        if (counter <= finalNumberOfTheCombination) {
             return counter;
         }
     }
 
-    let vocabTopics = Object.keys(vocab);
-    function getVocabSizes(){
+    function getVocabSizes() {
         return vocabTopics.map((topic) => vocab[topic].length);
     }
 
     let vocabSizes = getVocabSizes();
-    let gameMaxSeed = vocabSizes.reduce((acc, cur) => acc *= cur);
-
-    let NewGame = function(){
+    let seedEl = document.getElementById('seed');
+    let NewGame = function () {
         table.length = 1;
-        step=0;
+        step = 0;
         roundNumber = 0;
 
         table = [];
-        for(let i=0; i<table.length; i++){
+        for (let i = 0; i < table.length; i++) {
             table[i] = null;
         }
 
-        if(seedEl.value == ""){
-            seedEl.value = Math.floor(Math.random() * gameMaxSeed);
+        if (seedEl.value == "") {
+            seedEl.value = Math.floor(Math.random() * 50000);
+        }else{
+            seedEl.value = Math.min(Number.parseInt(seedEl.value), 50000);
         }
 
         seeds = calcSeeds(Number.parseInt(seedEl.value), vocabSizes);
@@ -273,12 +269,12 @@ function MemoryGame(){
         UpdateUI();
     }
 
-    this.toggleVisibility = function(){
+    this.toggleVisibility = function () {
         isVisible = !isVisible;
         UpdateUI();
     }
 
-    this.setSeed = function(){
+    this.setSeed = function () {
         NewGame();
     }
 
